d1bd7a68147c72b522c9dec3115f2fd5385e141b
Eric McKinney <mckinney99@gmail.com>
2017-10-20 19:00:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,13 @@
 def oxford_comma(array)
-  array.join("and")
-  index.each do |item|
-    puts "and"
-end
-end
+ if array.size == 1
+   array.join
+ elsif array.size < 3
+   array.join(" and ")
+ else
+   name = array.last
+   index = array.index(name)
+   array[index] = "and " + name
+   array.join(", ")
+ end
+       
+end 		 
